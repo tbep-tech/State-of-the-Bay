b58f432ecab249914d3bbb7dfbeb9da4d7ad6043
fawda123 <mbafs2012@gmail.com>
2021-04-19 08:26:41 -0400

    update seagrasscov figure

--- createfigs.R
+++ createfigs.R
@@ -209,11 +209,12 @@ lbs <- toplo$Year
 lbs[lbs %in% exyrs[-1]] <- ''
 
 p <- ggplot(toplo, aes(x = factor(Year), y = Acres)) +
-  with_shadow(geom_bar(aes(fill = fll), stat = 'identity', colour = 'black', width = 1.3), sigma = 2.5, x_offset = 0, y_offset = 0) +
+  with_shadow(geom_bar(aes(fill = fll), stat = 'identity', colour = 'black', width = 1.3), sigma = 2.7, x_offset = 0, y_offset = 0) +
   geom_segment(x = 0, xend = 2, y = 38, yend = 38, col = 'red', size = 2) +
   geom_segment(x = 4, xend = 42, y = 38, yend = 38, col = 'red', size = 2) +
   geom_segment(x = 42, xend = 44, y = 40, yend = 40, col = 'red', size = 2) +
   annotate("text", label = "Seagrass Coverage Goal", x = 4, y = 40.5, color = 'red', size = 5, hjust = 0, family = fml) +
+  annotate('text', x = 43, y = 34.5, label = 'Provisional: 35,240 acres', angle = 90, hjust = 1, vjust = 0.4) + 
   scale_x_discrete(breaks = lbs, labels = lbs, expand = c(0.04, 0.04)) +
   scale_fill_manual(values = c('#00806E', 'grey')) +
   scale_y_continuous(expand = c(0, 0), limits = c(0, 1.1 * max(toplo$Acres, na.rm = T))) +
@@ -221,7 +222,7 @@ p <- ggplot(toplo, aes(x = factor(Year), y = Acres)) +
   theme(
     axis.line = element_line(),
     panel.background = element_blank(),
-    axis.text.x = element_text(angle = 45, hjust = 1, size = 5.5),
+    axis.text.x = element_text(angle = 45, hjust = 1, size = 6),
     axis.title.x = element_blank(),
     legend.position = 'none',
     panel.grid.minor = element_blank(),
@@ -230,6 +231,7 @@ p <- ggplot(toplo, aes(x = factor(Year), y = Acres)) +
   labs(
     y = 'Seagrass Coverage (x1,000 acres)'
   )
+
 
 
 ##
@@ -262,7 +264,6 @@ gt$grobs[[is_axisr]] <-axisl$children[[1]]
 ##
 # save plot
 
-png('figures/seagrasscov.png', height = 3.25, width = 5, family = fml, res = 300, unit = 'in')
+png('figure/seagrasscov.png', height = 3.25, width = 6, res = 300, unit = 'in')
 grid.newpage(); grid.draw(gt)
 dev.off()
-
