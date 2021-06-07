ff3e3b962ac6bebcce70e2d36de3a309bd78b932
fawda123 <mbafs2012@gmail.com>
2021-06-07 11:47:13 -0400

    update seagrass coverage graphic with non-provisional results

--- createfigs.R
+++ createfigs.R
@@ -174,14 +174,6 @@ dev.off()
 ##
 # data prep
 
-seagrassadd <- seagrass %>%
-  bind_rows(
-    tibble(
-      Year = c(2020L),
-      Acres = c(35240)
-    )
-  )
-
 # extra years for padding
 exyrs <- seq(1950, 1953)
 
@@ -190,16 +182,23 @@ toplo <- tibble(
 ) %>%
   left_join(seagrass, by = 'Year') %>%
   mutate(
-    Acres = case_when(
-      Year == '2020' ~ 35240,
-      T ~ Acres
-    ),
-    Acres = Acres / 1000,
-    fll = case_when(
-      Year == '2020' ~ 'prelim',
-      T ~ 'historic'
-    )
+    Acres = Acres / 1000
   )
+
+# label for last bar 
+lastlab <- seagrass %>% 
+  filter(Year == max(Year)) %>% 
+  pull(Acres) %>% 
+  round(0) %>% 
+  format(big.mark = ',') %>% 
+  paste(., 'acres')
+ 
+# y loc for last bar label
+lasty <- seagrass %>% 
+  filter(Year == max(Year)) %>% 
+  pull(Acres) %>% 
+  `/`(1000) %>% 
+  `-`(1)
 
 ##
 # base ggplot
@@ -209,14 +208,13 @@ lbs <- toplo$Year
 lbs[lbs %in% exyrs[-1]] <- ''
 
 p <- ggplot(toplo, aes(x = factor(Year), y = Acres)) +
-  with_shadow(geom_bar(aes(fill = fll), stat = 'identity', colour = 'black', width = 1.3), sigma = 2.7, x_offset = 0, y_offset = 0) +
+  with_shadow(geom_bar(fill = '#00806E', stat = 'identity', colour = 'black', width = 1.3), sigma = 2.7, x_offset = 0, y_offset = 0) +
   geom_segment(x = 0, xend = 2, y = 38, yend = 38, col = 'red', size = 2) +
   geom_segment(x = 4, xend = 42, y = 38, yend = 38, col = 'red', size = 2) +
   geom_segment(x = 42, xend = 44, y = 40, yend = 40, col = 'red', size = 2) +
   annotate("text", label = "Seagrass Coverage Goal", x = 4, y = 40.5, color = 'red', size = 5, hjust = 0, family = fml) +
-  annotate('text', x = 43, y = 34.5, label = 'Provisional: 35,240 acres', angle = 90, hjust = 1, vjust = 0.4) + 
+  annotate('text', x = 43, y = lasty, label = lastlab, angle = 90, hjust = 1, vjust = 0.4) + 
   scale_x_discrete(breaks = lbs, labels = lbs, expand = c(0.04, 0.04)) +
-  scale_fill_manual(values = c('#00806E', 'grey')) +
   scale_y_continuous(expand = c(0, 0), limits = c(0, 1.1 * max(toplo$Acres, na.rm = T))) +
   # theme_bw() +
   theme(
@@ -231,8 +229,6 @@ p <- ggplot(toplo, aes(x = factor(Year), y = Acres)) +
   labs(
     y = 'Seagrass Coverage (x1,000 acres)'
   )
-
-
 
 ##
 # top, bottom axis line breaks
