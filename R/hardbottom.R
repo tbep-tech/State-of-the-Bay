c7a8fbff678c6533713625c1a71229395bfa98fd
fawda123 <mbafs2012@gmail.com>
2022-06-28 10:36:38 -0400

    hard bottom figs by bay segment

--- R/hardbottom.R
+++ R/hardbottom.R
@@ -4,6 +4,7 @@ library(ggplot2)
 library(tbeptools)
 library(here)
 library(showtext)
+library(dplyr)
 
 # get font
 font_add_google("Roboto", "roboto")#, regular = 'C:/Windows/Fonts/Roboto.ttf')
@@ -13,24 +14,39 @@ showtext_auto()
 showtext_opts(dpi = 500)
 
 load(url('https://github.com/tbep-tech/hmpu-workflow/raw/master/data/hard.RData'))
-hard <- st_make_valid(hard)# %>% 
+hard <- st_make_valid(hard) %>% 
+  st_transform(crs = st_crs(tbseg)) %>% 
+  st_intersection(., tbseg) %>% 
+  mutate(long_name = factor(long_name, levels = c('Old Tampa Bay', 'Middle Tampa Bay', 'Lower Tampa Bay')))
   # st_buffer(dist = 30)
 
-subttl <- paste(round(sum(hard$Acres), 0), 'Acres')
-p <- ggplot() +
-  annotation_map_tile(zoom = 11, type = 'cartolight') +
-  geom_sf(data = tbseg, color = NA, fill = NA) + 
-  geom_sf(data = hard, color = 'red', fill = 'red') + 
-  theme_minimal(base_family = fml) + 
-  theme(
-    axis.text= element_blank(), 
-    axis.ticks = element_blank()
-  ) + 
-  labs(
-    title = "Hard-bottom habitat in Tampa Bay", 
-    subtitle = subttl
-  )
+for(lev in levels(hard$long_name)){
+  
+  tomap <- hard %>% 
+    filter(long_name %in% lev)
+  seg <- tbseg %>% 
+    filter(long_name %in% lev)
+  ttl <- paste('Hard Bottom in', lev)
+  subttl <- paste(round(sum(tomap$Acres), 0), 'Acres')
+  levshrt <- unique(tomap$bay_segment)
+  flnm <- here(paste0('figures/hardbottom', levshrt, '.jpg'))
+  
+  p <- ggplot() +
+    annotation_map_tile(zoom = 11, type = 'cartolight') +
+    geom_sf(data = seg, color = NA, fill = NA) + 
+    geom_sf(data = tomap, color = 'red', fill = 'red') + 
+    theme_minimal(base_family = fml) + 
+    theme(
+      axis.text= element_blank(), 
+      axis.ticks = element_blank()
+    ) + 
+    labs(
+      title = ttl, 
+      subtitle = subttl
+    )
+  
+  jpeg(flnm, family = fml, height = 5, width = 5, res = 500, units = 'in')
+  print(p)
+  dev.off()
 
-jpeg(here('figures/hardbottom.jpg'), family = fml, height = 5, width = 3.1, res = 500, units = 'in')
-print(p)
-dev.off()
+}
